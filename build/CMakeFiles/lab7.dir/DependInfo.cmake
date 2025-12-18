6d54247571c3b2a8b5aa87addc6cae93354b9e24
Andrey Denisov <and.denisov26@gmail.com>
2025-12-18 16:43:59 +0300

    build

---
+++ build/CMakeFiles/lab7.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andrey/LabsOOP/AsyncOOP/src/game.cpp" "CMakeFiles/lab7.dir/src/game.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/game.cpp.o.d"
+  "/home/andrey/LabsOOP/AsyncOOP/src/knight.cpp" "CMakeFiles/lab7.dir/src/knight.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/knight.cpp.o.d"
+  "/home/andrey/LabsOOP/AsyncOOP/src/main.cpp" "CMakeFiles/lab7.dir/src/main.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/main.cpp.o.d"
+  "/home/andrey/LabsOOP/AsyncOOP/src/npc.cpp" "CMakeFiles/lab7.dir/src/npc.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/npc.cpp.o.d"
+  "/home/andrey/LabsOOP/AsyncOOP/src/pegasus.cpp" "CMakeFiles/lab7.dir/src/pegasus.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/pegasus.cpp.o.d"
+  "/home/andrey/LabsOOP/AsyncOOP/src/squirrel.cpp" "CMakeFiles/lab7.dir/src/squirrel.cpp.o" "gcc" "CMakeFiles/lab7.dir/src/squirrel.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lab7.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab7.dir/src/game.cpp.o"
+  "CMakeFiles/lab7.dir/src/game.cpp.o.d"
+  "CMakeFiles/lab7.dir/src/knight.cpp.o"
+  "CMakeFiles/lab7.dir/src/knight.cpp.o.d"
+  "CMakeFiles/lab7.dir/src/main.cpp.o"
+  "CMakeFiles/lab7.dir/src/main.cpp.o.d"
+  "CMakeFiles/lab7.dir/src/npc.cpp.o"
+  "CMakeFiles/lab7.dir/src/npc.cpp.o.d"
+  "CMakeFiles/lab7.dir/src/pegasus.cpp.o"
+  "CMakeFiles/lab7.dir/src/pegasus.cpp.o.d"
+  "CMakeFiles/lab7.dir/src/squirrel.cpp.o"
+  "CMakeFiles/lab7.dir/src/squirrel.cpp.o.d"
+  "lab7"
+  "lab7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
